58d4367902b7b03247a302e3e59c0bafa659b0a2
sergxlove <sergxlove@mail.ru>
2024-05-08 23:54:18 +0600

    start work under mouses

---
+++ resourse/Mouse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergxloveCoin.resourse
+{
+    internal abstract class Mouse
+    {
+        public abstract string Name { get; set; }
+        public abstract float Price { get; set; }
+        public abstract float SpeedClick { get; set; }
+        public abstract int Qantity { get; set; }
+        protected string _name;
+        protected float _price;
+        protected float _speedClick;
+        protected int _quantity;
+    }
+    internal sealed class CommonMouse : Mouse , INotifyPropertyChanged
+    {
+        public CommonMouse() { }
+        public override string Name
+        {
+            get => _name;
+            
+        }
+        public override void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
