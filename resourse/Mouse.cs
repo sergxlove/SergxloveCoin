47a18ca7c2033737fefdb7d8e2dbbeb2b971a6e3
sergxlove <sergxlove@mail.ru>
2024-05-09 23:37:51 +0600

    add one object mouse

--- resourse/Mouse.cs
+++ resourse/Mouse.cs
@@ -19,17 +19,61 @@ namespace SergxloveCoin.resourse
         protected float _speedClick;
         protected int _quantity;
     }
-    internal sealed class CommonMouse : Mouse , INotifyPropertyChanged
+    internal sealed class CommonMouse : Mouse, INotifyPropertyChanged
     {
-        public CommonMouse() { }
+        public CommonMouse()
+        {
+            _name = "Обычная мышь";
+            _price = 0.500f;
+            _speedClick = 0.003f;
+            _quantity = 0;
+        }
+        public CommonMouse(string name, float price, float speedClick, int quantity)
+        {
+            _name = name;
+            _price = price;
+            _speedClick = speedClick;
+            _quantity = quantity;
+        }
         public override string Name
         {
             get => _name;
-            
+            set
+            {
+                _name = value;
+            }
         }
-        public override void OnPropertyChanged([CallerMemberName] string prop = "")
+        public override float Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                OnPropertyChanged();
+            }
+        }
+        public override float SpeedClick
+        {
+            get => _speedClick;
+            set
+            {
+                _speedClick = value;
+                OnPropertyChanged();
+            }
+        }
+        public override int Qantity
+        {
+            get => _quantity;
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged();
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
