129bdc6314593e0083db0dbf4456560556e1f445
sergxlove <sergxlove@mail.ru>
2024-05-11 14:01:05 +0600

    add new abstract claass processor

---
+++ resourse/Processor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergxloveCoin.resourse
+{
+    internal abstract class Processor
+    {
+        public abstract string Name { get; set; }
+        public abstract float Price { get; set; }
+        public abstract float Speed { get; set; }
+        public abstract int Quantity { get; set; }
+        protected string? _name;
+        protected float _price;
+        protected float _speed;
+        protected int _quantity;
+    }
+}
